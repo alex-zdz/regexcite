6cb80e24c210090de11967c61fdf1047471f1ce6
alex-zdz <63648770+alex-zdz@users.noreply.github.com>
2022-06-13 17:16:00 +0200

    Editing the DESCRIPTION file using ctrl . "DESCRIPTION". Adding a license using use_mit_license(). Adding Namespace with Roxygen.

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Title
+#'
+#' @param x A character vector
+#' @param split  what to split on
+#'
+#' @return  Return splitted character
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
